becb6778090f6740160bc3b385c190adf75ea40d
Weijie Lin <weijie@gmail.com>
2014-06-30 16:56:05 -0700

    basic works

---
+++ RealDocLib/TestMethodNameParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealDocLib
+{
+    using System.IO;
+    using System.Reflection;
+
+    public class TestMethodNameParser
+    {
+        private readonly string dllFullPath;
+
+        public TestMethodNameParser(string dllFullPath)
+        {
+            if (dllFullPath == null)
+            {
+                throw new ArgumentNullException("dllFullPath");
+            }
+
+            this.dllFullPath = dllFullPath;
+        }
+
+        public IDictionary<string, IList<string>> Parse()
+        {
+            var assembly = Assembly.LoadFile(this.dllFullPath);
+            var testMethods = new Dictionary<string, IList<string>>();
+            foreach (var classType in assembly.GetTypes())
+            {
+                var customAttributes = classType.GetCustomAttributes(true);
+                foreach (var customAttribute in customAttributes)
+                {
+                    if (customAttribute.GetType().Name == "TestClassAttribute")
+                    {
+                        var nameList = new List<string>();
+                        testMethods.Add(classType.FullName, nameList);
+                        foreach (var methodInfo in classType.GetMethods())
+                        {
+                            nameList.Add(methodInfo.Name);                          
+                        }
+                    }
+                }
+            }
+
+            return testMethods;
+        }
+    }
+}
--- RealDocProgram/Program.cs
+++ RealDocProgram/Program.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace RealDocProgram
 {
+    using RealDocLib;
+
     class Program
     {
         static void Main(string[] args)
         {
+            var parser = new TestMethodNameParser(args[0]);
+            var dictionary = parser.Parse();
+            foreach (var className in dictionary)
+            {
+                Console.WriteLine(className.Key);
+                foreach (var methodName in className.Value)
+                {
+                    Console.Write("\t");
+                    Console.WriteLine(methodName);
+                }
+            }
         }
     }
 }
